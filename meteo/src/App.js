9f38748f4a108f1ffbef5d697797a8b6dc1485f0
olivier Tristan <tristanolivier.work@gmail.com>
2019-05-15 16:08:00 +0200

    clean react app

--- meteo/src/App.js
+++ meteo/src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+         
+          <h2>Meteo-App</h2>
+        </div>
+        <p className="App-intro">
+          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+  }
 }
 
 export default App;
